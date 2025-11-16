e0f034782a86689e58e75a30260296f8fddd3ef7
mgaillard <gaillard.mathieu.39@gmail.com>
2025-11-15 18:41:41 -0800

    Tokenizer support for special tokens

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -16,6 +16,16 @@ class Tokenizer:
         self.inv_vocab: dict[bytes, int] = {v: k for k, v in vocab.items()}
         # A hashmap for the merge table for faster lookup
         self.merge_table: dict[tuple[bytes, bytes], int] = {pair: rank for rank, pair in enumerate(merges)}
+        # If any of the special tokens don't exist in the vocab, append them to the vocab.
+        if self.special_tokens:
+            # Sort the special tokens by length in descending order to avoid sub-token issues
+            self.special_tokens.sort(key=lambda x: len(x), reverse=True)
+            for special_token in self.special_tokens:
+                byte_encoded_special_token = special_token.encode("utf-8")
+                if byte_encoded_special_token not in self.inv_vocab:
+                    index = len(vocab) # Warning: This assumes that the vocab is a dense mapping from 0 to N-1
+                    self.vocab[index] = byte_encoded_special_token
+                    self.inv_vocab = index
 
     @classmethod
     def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
@@ -24,6 +34,34 @@ class Tokenizer:
         (in the same format that your BPE training code output) and (optionally) a list of special tokens.
         """
         pass
+
+    def _convert_bytes_to_token(self, byte_seq: bytes) -> int:
+        """
+        Convert a byte sequence to its corresponding token ID using the vocabulary.
+        """
+        if byte_seq in self.inv_vocab:
+            return self.inv_vocab[byte_seq]
+        else:
+            raise ValueError(f"Byte sequence {byte_seq} not found in vocabulary")
+        
+    def _find_next_special_token(self, text: str, start_pos: int, end_pos: int) -> tuple[int, str]:
+        """
+        Find the next special token in the text between start_pos and end_pos.
+        Returns the index of the next special token, or -1 if none found.
+        """
+        if not self.special_tokens:
+            return -1
+        
+        next_special_token: str = ""
+        next_special_token_pos = -1
+        for special_token in self.special_tokens:
+            index = text.find(special_token, start_pos, end_pos)
+            if index != -1:
+                if next_special_token_pos == -1 or index < next_special_token_pos:
+                    next_special_token = special_token
+                    next_special_token_pos = index
+        
+        return next_special_token_pos, next_special_token
 
     def _encode_pretoken(self, input_pretoken: bytes) -> list[int]:
         """
@@ -67,12 +105,7 @@ class Tokenizer:
         # convert final tokens to token IDs using self.vocab
         token_ids: list[int] = []
         for token in current_tokens:
-            if token in self.inv_vocab:
-                token_ids.append(self.inv_vocab[token])
-                continue
-            else:
-                # Token not found in vocabulary, handle unknown token case
-                raise ValueError(f"Token {token} not found in vocabulary")
+            token_ids.append(self._convert_bytes_to_token(token))
 
         return token_ids
 
@@ -82,23 +115,46 @@ class Tokenizer:
         """
         tokens: list[int] = []
 
-        # TODO: remove special tokens from text if any are present
+        pos = 0
+        while pos < len(text):
+            # By default, we intend to process until the end of the text
+            endpos = len(text)
 
-        # The regex splitter will split the text into pre-tokens (words, punctuation, spaces, etc.)
-        for m in re.finditer(self.regex_splitter, text):
-            # Convert the pre-token to UTF-8 bytes
-            pre_token_str = m.group(0)
-            pre_token_utf8_encoded = pre_token_str.encode("utf-8")
-            # Encode the pre-token using BPE and add the tokens to the output list
-            tokens.extend(self._encode_pretoken(pre_token_utf8_encoded))
-        
+            # But we attempt to find the first special token in the text, and if found, we will process until there
+            if self.special_tokens:
+               index, special_token_str = self._find_next_special_token(text, pos, endpos)
+               if index != -1:
+                   if index == pos:
+                        # Special token found at the current position
+                        special_token_utf8_encoded = special_token_str.encode("utf-8")
+                        # We add the special token ID directly to the output tokens
+                        tokens.append(self._convert_bytes_to_token(special_token_utf8_encoded))
+                        # We move the position forward by the length of the special token
+                        pos += len(special_token_str)
+                        continue
+                   else:
+                       # Special token found later in the text, process until the next special token
+                       endpos = index
+
+            # The regex splitter will split the text into pre-tokens (words, punctuation, spaces, etc.)
+            for m in re.finditer(self.regex_splitter, text, pos=pos, endpos=endpos):
+                # This is the pre-token found by the regex
+                pre_token_str = m.group(0)
+                # Convert the pre-token to UTF-8 bytes
+                pre_token_utf8_encoded = pre_token_str.encode("utf-8")
+                # Encode the pre-token using BPE and add the tokens to the output list
+                tokens.extend(self._encode_pretoken(pre_token_utf8_encoded))
+
+            pos = endpos
+            
         return tokens
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         """
         Encode an iterable of strings into a flat iterator of token IDs.
         """
-        pass
+        for text in iterable:
+            yield from self.encode(text)
     
     def decode(self, ids: list[int]) -> str:
         """
