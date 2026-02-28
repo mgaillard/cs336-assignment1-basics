5eab388ded62ad65e9cb4635ca46b94f541322e0
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2026-02-27 20:34:22 -0800

    Add files for LR experiment

---
+++ configs/experiments/lr/run.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+#
+# Run this file with:
+# nohup bash configs/experiments/lr/run.sh > lr_experiments.log 2>&1 &
+# And check TensorBoard
+#
+uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_00003.yaml
+uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_0001.yaml
+uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_0003.yaml
+uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_001.yaml
+uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_003.yaml
