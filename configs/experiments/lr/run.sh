ab75c1bdbeb985c35a3aa3ec63241a98323c4186
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2026-02-27 20:38:39 -0800

    Typo

--- configs/experiments/lr/run.sh
+++ configs/experiments/lr/run.sh
@@ -4,8 +4,8 @@
 # nohup bash configs/experiments/lr/run.sh > lr_experiments.log 2>&1 &
 # And check TensorBoard
 #
-uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_00003.yaml
-uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_0001.yaml
-uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_0003.yaml
-uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_001.yaml
-uv run cs336_basics/train.py --config configs/exeriments/lr/gpt_small_lr_003.yaml
+uv run cs336_basics/train.py --config configs/experiments/lr/gpt_small_lr_00003.yaml
+uv run cs336_basics/train.py --config configs/experiments/lr/gpt_small_lr_0001.yaml
+uv run cs336_basics/train.py --config configs/experiments/lr/gpt_small_lr_0003.yaml
+uv run cs336_basics/train.py --config configs/experiments/lr/gpt_small_lr_001.yaml
+uv run cs336_basics/train.py --config configs/experiments/lr/gpt_small_lr_003.yaml
